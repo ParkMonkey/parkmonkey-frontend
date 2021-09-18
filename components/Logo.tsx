9a23c0846af64617502d0ea806640fcb6a963f0f
Vamiq <vamiq.valji@gmail.com>
2021-09-18 12:32:11 -0400

    fix image sizing

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -20,7 +20,7 @@ const Logo: React.FC<LogoProps> = ({
     <>
         <Image 
             source={require('../assets/images/green_logo_small.png')} 
-            style={size ? {width:size.width, height:size.height} : {width: 32, height: 32}}
+            style={{width:size.width, height:size.height, resizeMode:'contain'}}
         />
     </>);
 }
