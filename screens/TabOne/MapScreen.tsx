b0c87e5c2eb6020614cc7e7262d5c9a3f8616500
Vamiq <vamiq.valji@gmail.com>
2021-09-19 02:16:07 -0400

    location data

--- screens/TabOne/MapScreen.tsx
+++ screens/TabOne/MapScreen.tsx
@@ -3,15 +3,54 @@ import Vew from '../../components/custom/Vew';
 import MapView from 'react-native-maps';
 import { StyleSheet, Dimensions } from 'react-native';
 
+interface MapLocationsInterface {
+  address: string,
+  postal: string,
+  coords: { lat: number, long: number }
+}
+
+const locations:MapLocationsInterface[] = [
+  {
+    address: "262 Jarvis St",
+    postal: "Toronto, Ontario M5B 2B9",
+    coords: { lat: 43.659944814129624, long: -79.37613383557787 }
+  },
+  {
+    address: "394 Dovercourt Rd",
+    postal: "Toronto, Ontario M6J 3E7",
+    coords: { lat: 43.6525110595773, long: -79.4261250017898 }
+  },
+  {
+    address: "550 Jarvis St",
+    postal: "Toronto, Ontario M4Y 2H9",
+    coords: { lat: 43.66936020876486, long: -79.3799488017894 }
+  },
+  {
+    address: "70 Temperance St",
+    postal: "Toronto, Ontario M5H 0B1",
+    coords: { lat: 43.650693101753525, long: -79.38216965946145 }
+  },
+  {
+    address: "218 Queen Street East",
+    postal: "Toronto, Ontario M5A 1S3",
+    coords: { lat: 43.65484329048784, long: -79.3692181729539 }
+  },
+  {
+    address: "394 Dovercourt Road",
+    postal: "Toronto, Ontario M6J 3E5",
+    coords: { lat: 43.65247224588065, long: -79.4262537478195 }
+  },
+]
+
 interface MapScreenProps {
-    navigation?: any
+  navigation?: any
 }
 
 const MapScreen: React.FC<MapScreenProps> = ({navigation}) => {
     return (
     <>
       <Vew style={styles.container}>
-          <MapView style={styles.map} />
+        <MapView style={styles.map} />
       </Vew>
     </>);
 }
