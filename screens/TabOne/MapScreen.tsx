cfc9f89d5bcb5a94d4c588f61c001ba3be3ab1d9
Vamiq <vamiq.valji@gmail.com>
2021-09-19 03:13:48 -0400

    marker banana points with prices

--- screens/TabOne/MapScreen.tsx
+++ screens/TabOne/MapScreen.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Vew from '../../components/custom/Vew';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Dimensions, Image } from 'react-native';
+import { StyleSheet, Dimensions, Image, ImageStyle, StyleProp } from 'react-native';
 
 interface MapLocationsInterface {
   address: string,
@@ -14,11 +14,6 @@ const locations:MapLocationsInterface[] = [
     address: "262 Jarvis St",
     postal: "Toronto, Ontario M5B 2B9",
     coords: { lat: 43.659944814129624, long: -79.37613383557787 }
-  },
-  {
-    address: "394 Dovercourt Rd",
-    postal: "Toronto, Ontario M6J 3E7",
-    coords: { lat: 43.6525110595773, long: -79.4261250017898 }
   },
   {
     address: "550 Jarvis St",
@@ -35,20 +30,34 @@ const locations:MapLocationsInterface[] = [
     postal: "Toronto, Ontario M5A 1S3",
     coords: { lat: 43.65484329048784, long: -79.3692181729539 }
   },
-  {
-    address: "394 Dovercourt Road",
-    postal: "Toronto, Ontario M6J 3E5",
-    coords: { lat: 43.65247224588065, long: -79.4262537478195 }
-  },
-  {
-    address: "Placeholder",
-    postal: "Placeholder",
-    coords: { lat: 43.65484329048789, long: -79.369218172954 }
-  },
 ]
 
 interface MapScreenProps {
   navigation?: any
+}
+
+// const getBananaPath = (idx:number) => {
+//   if (idx === 0) {
+//     return require("../../assets/images/banana_4.png");
+//   } else if (idx === 1) {
+//     return require("../../assets/images/banana_1.png");
+//   } else {
+//     return require("../../assets/images/banana_2.png");
+//   }
+// }
+
+const getBananaImgs = (idx:number) => {
+  const style:StyleProp<ImageStyle>={
+    width: 64, height: 64,
+  };
+
+  if (idx === 0) {
+    return <Image source={require("../../assets/images/banana_4.png")} style={style} />;
+  } else if (idx === 1) {
+    return <Image source={require("../../assets/images/banana_1.png")} style={style} />;
+  } else {
+    return <Image source={require("../../assets/images/banana_2.png")} style={style} />;
+  }
 }
 
 const MapScreen: React.FC<MapScreenProps> = ({navigation}) => {
@@ -59,7 +68,7 @@ const MapScreen: React.FC<MapScreenProps> = ({navigation}) => {
           region={{
             latitude: 43.659944814129624,
             longitude: -79.37613383557787,
-            latitudeDelta: 0.03,
+            latitudeDelta: 0.032,
             longitudeDelta: 0.01,
           }}
         >
@@ -69,9 +78,7 @@ const MapScreen: React.FC<MapScreenProps> = ({navigation}) => {
               coordinate={{latitude: coords.lat, longitude: coords.long}}
               // title={address}
             >
-              <Image source={require("../../assets/images/banana.png")} style={{
-                width: 64, height: 64,
-              }} />
+              {getBananaImgs(idx)}
             </Marker>
           ))]}
 
