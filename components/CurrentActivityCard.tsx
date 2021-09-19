f1347e7f3db2de76cc7a9e8a78bfdc313030dbc9
Simon Zhang <simonzhang0422@gmail.com>
2021-09-18 21:00:56 -0400

    Activity Screen part 1 done

--- components/CurrentActivityCard.tsx
+++ components/CurrentActivityCard.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
-export default function ActivityCard(props) {
+export default function CurrentActivityCard(props) {
     return (
         <View style={styles.container}>
             <Text style={styles.date}>{props.date}</Text>
             <View>
-              <Text style={styles.time}>You Parked {props.time} min</Text>
+              <Text style={styles.time}>Parking from {props.startTime} to {props.endTime}</Text>
               <Text style={styles.address}>at {props.address}</Text>
             </View>
         </View>
@@ -16,7 +16,7 @@ export default function ActivityCard(props) {
   
   const styles = StyleSheet.create({
     container: {
-      backgroundColor: 'black',
+      backgroundColor: '#1ECF65',
       width: 600,
       padding: 20,
       borderRadius: 30,
--- screens/ActivityScreen.tsx
+++ screens/ActivityScreen.tsx
@@ -1,13 +1,36 @@
 import * as React from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, ScrollView, SafeAreaView} from 'react-native';
+import ActivityCard from '../components/ActivityCard';
+import CurrentActivityCard from '../components/CurrentActivityCard';
 import ScreenTitle from '../components/ScreenTitle';
 
 export default function ActivityScreen() {
     return (
-        <ScreenTitle title="Activity" subtext="Take a look at your" path={require("../assets/images/history.png")} />
+        <ScrollView bounces={false} >
+        <View style={styles.container}>
+            <ScreenTitle title="Activity" subtext="Take a look at your" path={require("../assets/images/history.png")} />
+                <Text style={styles.title}>Current Activity</Text>
+                <CurrentActivityCard startTime='11AM' endTime='7PM' date='11/09/21' address='6979 Greenrose Dr.'/>
+                <Text style={styles.title}>Past Activity</Text>
+                <ActivityCard time='60' date='11/09/21' address='451 Churchill St.'/>
+                <ActivityCard time='120' date='13/09/21' address='593 Erindate Rd.'/>
+                <ActivityCard time='60' date='11/09/21' address='2334 Hadock St.'/>
+        </View>
+        </ScrollView>
       );
   }
   
   const styles = StyleSheet.create({
+      container: {
+          display: 'flex',
+          alignItems: 'center',
+      },
+      title: {
+          fontFamily: 'josefin',
+          fontSize: 50,
+          textAlign: 'center',
+          fontWeight: 'bold',
+          marginBottom: 30,
+      }
   });
   
