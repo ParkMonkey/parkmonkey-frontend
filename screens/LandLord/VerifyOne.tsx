8958630304929a50ea971a8a94af8c28ea6ff5cb
Vamiq <vamiq.valji@gmail.com>
2021-09-19 07:47:08 -0400

    comps

---
+++ screens/LandLord/VerifyOne.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Dimensions, Image, TouchableOpacity } from "react-native";
+import { ScrollView } from 'react-native-gesture-handler';
+import Vew from '../../components/custom/Vew';
+
+interface PaymentOneProps {
+    navigation?: any
+}
+
+const VerifyOne: React.FC<PaymentOneProps> = ({navigation}) => {
+    return (
+    <>
+        <ScrollView contentContainerStyle={{paddingBottom:-5000}}>
+            <TouchableOpacity onPress={() => navigation.navigate("VerifyTwo")}>
+                <Vew w={Dimensions.get('window').width} flex={1} justifyContent="center" alignItems="center">
+                    <Image 
+                        source={require('../../assets/images/Verify_1.png')}
+                        style={{resizeMode: 'contain', height: 1900}}
+                    />
+                </Vew>
+            </TouchableOpacity>
+        </ScrollView>
+    </>);
+}
+
+export default VerifyOne;
