5915b37f79e719dfd8693aa33db827a9b27dc91e
Simon Zhang <simonzhang0422@gmail.com>
2021-09-19 01:12:02 -0400

    LandlordUnVerified screen (route button later)

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -14,6 +14,7 @@ import LinkingConfiguration from './LinkingConfiguration';
 import ActivityScreen from '../screens/ActivityScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import MessagesMainScreen from '../screens/MessagesMainScreen';
+import LandlordUnverifiedScreen from '../screens/LandlordUnverifiedScreen';
 import Txt from '../components/custom/Txt';
 import { Image } from "react-native";
 import { useAuth } from '../context/AuthContext';
@@ -108,7 +109,7 @@ function BottomTabNavigator() {
             { name: "Dashboard", component: TabOneScreen, icon: "bars" },
             { name: "Search", component: TabOneScreen, icon: "map" },
             { name: "Activity", component: ActivityScreen, icon: "clock-o" },
-            { name: "Landlord", component: TabOneScreen, icon: "home" },
+            { name: "Landlord", component: LandlordUnverifiedScreen, icon: "home" },
             { name: "Message", component: MessagesMainScreen, icon: "comments" },
             { name: "Settings", component: SettingsScreen, icon: "cog" },
           ].map(({name, component, icon}, idx) => (
