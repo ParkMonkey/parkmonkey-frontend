5af1edf9c85243df20d3fb31f7f5c53a0294fb51
Vamiq <vamiq.valji@gmail.com>
2021-09-19 01:51:09 -0400

    map view working!

--- screens/TabOne/MapScreen.tsx
+++ screens/TabOne/MapScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Vew from '../../components/custom/Vew';
+import MapView from 'react-native-maps';
+import { StyleSheet, Dimensions } from 'react-native';
 
 interface MapScreenProps {
     navigation?: any
@@ -8,10 +10,23 @@ interface MapScreenProps {
 const MapScreen: React.FC<MapScreenProps> = ({navigation}) => {
     return (
     <>
-        <Vew>
-            
-        </Vew>
+      <Vew style={styles.container}>
+          <MapView style={styles.map} />
+      </Vew>
     </>);
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+  });
 
 export default MapScreen;
